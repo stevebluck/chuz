2321d62a03ffb1b1071d4dd440ed0e9c035a84ae
Stephen Bluck <stephenbluck@msn.com>
2024-05-18 13:22:02 +0100

    remove unused hooks

--- web/src/hooks/useActionData.ts
+++
@@ -1,6 +0,0 @@
-import { useActionData as remixUseActionData } from "@remix-run/react";
-
-export const useActionData = () => {
-  const data = remixUseActionData<Record<string, string | undefined>>();
-  return data || null;
-};
